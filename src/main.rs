42c049ff3d44b5c043ef1127744a6bfbf1e2b6e7
Andrew Doble <andrew.l.doble@gmail.com>
2023-06-19 14:47:38 +0200

    Passed test of function copy_file

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,7 @@ fn main() {
     let copy_action = is_newer(&source_path, &backup_path);
 
     if copy_action {
-        if let Err(err) = backup_file(&source_path, &backup_path) {
+        if let Err(err) = copy_file(&source_path, &backup_path) {
             eprintln!("Error copying file: {}", err);
         } else {
             println!("File copied successfully.");
@@ -80,7 +80,7 @@ fn is_newer(source_file: &PathBuf, backup_file: &std::path::PathBuf) -> bool {
     false
 }
 
-fn backup_file(source_file_path: &PathBuf, backup_file_path: &PathBuf) -> io::Result<()> {
+fn copy_file(source_file_path: &PathBuf, backup_file_path: &PathBuf) -> io::Result<()> {
     // Open the source file for reading
     let mut source_file_content = Vec::new();
     let mut source_file = fs::File::open(source_file_path)?;
@@ -101,11 +101,13 @@ fn backup_file(source_file_path: &PathBuf, backup_file_path: &PathBuf) -> io::Re
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     use std::fs::File;
     use std::io::{self, Write};
     //use tempfile::tempdir;
 
-    use crate::is_newer;
+    //use crate::is_newer;
 
     #[test]
     fn test_is_newer_where_backup_file_does_not_exist() -> Result<(), std::io::Error> {
@@ -179,6 +181,69 @@ mod tests {
         //Cleanup
         drop(source_file);
         drop(backup_file);
+        test_dir.close()?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_first_backup() -> Result<(), std::io::Error> {
+        // Set up test data
+        let test_content = "Some test content".to_string();
+
+        let test_dir = tempfile::tempdir()?;
+
+        let source_path = test_dir.path().join("source_test_data");
+        let mut source_file = File::create(source_path)?;
+        write!(source_file, "{}", test_content)?;
+
+        // Test the back_file function
+        let backup_path = test_dir.path().join("backup");
+        let source_path_created = test_dir.path().join("source_test_data");
+        copy_file(&source_path_created, &backup_path)?;
+
+        let mut created_backup_file = File::open(backup_path)?;
+        let mut buf = String::new();
+        created_backup_file.read_to_string(&mut buf)?;
+        assert_eq!(buf, test_content);
+
+        //Cleanup
+        drop(source_file);
+        test_dir.close()?;
+
+        Ok(())
+    }
+    #[test]
+    fn test_subsequent_backup() -> Result<(), std::io::Error> {
+        // Set up test data
+
+        let test_dir = tempfile::tempdir()?;
+
+        let backup_path = test_dir.path().join("backup");
+        let mut backup_file = File::create(backup_path)?;
+        write!(
+            backup_file,
+            "Some really old data that should be overwritten."
+        )?;
+        assert!(test_dir.path().join("backup").exists());
+
+        let test_content = "Some test content".to_string();
+        let source_path = test_dir.path().join("source_test_data");
+        let mut source_file = File::create(source_path)?;
+        write!(source_file, "{}", test_content)?;
+
+        // Test the copy_file function
+        let backup_path = test_dir.path().join("backup");
+        let source_path_created = test_dir.path().join("source_test_data");
+        copy_file(&source_path_created, &backup_path)?;
+
+        let mut created_backup_file = File::open(backup_path)?;
+        let mut buf = String::new();
+        created_backup_file.read_to_string(&mut buf)?;
+        assert_eq!(buf, test_content);
+
+        //Cleanup
+        drop(source_file);
         test_dir.close()?;
 
         Ok(())
