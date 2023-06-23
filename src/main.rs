3cfe889f5d5cb590a45be189b56894320ea4b7e9
Andrew Doble <andrew.l.doble@gmail.com>
2023-06-23 17:24:07 +0200

    Able to create new backup files

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,10 @@ fn main() {
 
     let backup_dir_path = cli.backup; // TODO as environment variable
 
+    perform_backup(source_dir_path, backup_dir_path);
+}
+
+fn perform_backup(source_dir_path: PathBuf, backup_dir_path: PathBuf) {
     // NOTE: This can be shortened to `WalkerConfig::new(vec![])`
     //       (expanded here for explanations purpose)
     let config = WalkerConfig {
@@ -52,23 +56,34 @@ fn main() {
     let source_files_list =
         walk(&source_dir_path, &config).expect("Failed to build the files list");
 
-    let mut components = source_files_list[0].components();
-    for c in components {
-        println!("component:{:?}", c);
-    }
+    // let mut components = source_files_list[0].components();
+    // for c in components {
+    //     println!("component:{:?}", c);
+    // }
 
     for source_file_path in source_files_list {
         let backup_file_path = create_backup_file_path(&source_file_path, &backup_dir_path);
+
         if is_newer(&source_file_path, &backup_file_path) {
-            if let Err(err) = copy_file(&source_dir_path, &backup_file_path) {
-                eprintln!("Error copying file: {}", err);
+            if let Err(err) = copy_file(&source_file_path, &backup_file_path) {
+                eprintln!(
+                    "Error copying {}: {}",
+                    source_file_path.to_string_lossy(),
+                    err
+                );
             } else {
-                println!("File copied successfully.");
+                println!(
+                    "File {} copied successfully.",
+                    source_file_path.to_string_lossy()
+                );
             }
         }
     }
 }
 
+/// Checks if the `source_file`is newer then the `backup_file`.
+/// If the `backup_file`does not exist then this always returns `true`.
+///
 fn is_newer(source_file: &PathBuf, backup_file: &std::path::PathBuf) -> bool {
     // Check if the backup file exists. If it does not return true as the source file
     // is "newer"
@@ -89,29 +104,41 @@ fn is_newer(source_file: &PathBuf, backup_file: &std::path::PathBuf) -> bool {
     false
 }
 
+/// Copies over the backup file.
 fn copy_file(source_file_path: &PathBuf, backup_file_path: &PathBuf) -> io::Result<()> {
-    // Open the source file for reading
-    let mut source_file_content = Vec::new();
-    let mut source_file = fs::File::open(source_file_path)?;
-    source_file.read_to_end(&mut source_file_content)?;
+    // Create the directory/directories the file is in if they have not already been created.
+    let mut dir = backup_file_path.clone();
+    dir.pop();
+    fs::create_dir_all(dir)?;
 
-    // Create or open the existing file for writing
-    let mut backup_file = fs::OpenOptions::new()
-        .create(true)
-        .truncate(true)
-        .write(true)
-        .open(backup_file_path)?;
+    // Open the source file for reading, but only if it is a file
+    // (directories hve been created before).
+    if source_file_path.is_file() {
+        let mut source_file_content = Vec::new();
+        let mut source_file = fs::File::open(source_file_path)?;
+        source_file.read_to_end(&mut source_file_content)?;
 
-    // Write the contents of the checked file to the existing file
-    backup_file.write_all(&source_file_content)?;
+        // Create or open the existing file for writing
+        let mut backup_file = fs::OpenOptions::new()
+            .create(true)
+            .truncate(true)
+            .write(true)
+            .open(backup_file_path)?;
+
+        // Write the contents of the checked file to the existing file
+        backup_file.write_all(&source_file_content)?;
+    } else {
+        // Is just a directory so create it
+        fs::create_dir_all(backup_file_path)?;
+    }
 
     Ok(())
 }
 
 // Create the path of the file being backed up, i.e.:
-// With source file: C:/Users/bob/Documents/test.txt
+// with source file: C:/Users/bob/Documents/test.txt
 // and backup directory C:/Users/bob/Backup it will create a PathBuf of
-//      C:/Users/bob/Backup/c/Users/bob/Documents/test.txt
+//      C:/Users/bob/Backup/C/Users/bob/Documents/test.txt
 
 fn create_backup_file_path(source_file_path: &PathBuf, backup_dir_path: &PathBuf) -> PathBuf {
     let components = source_file_path.components();
@@ -124,7 +151,8 @@ fn create_backup_file_path(source_file_path: &PathBuf, backup_dir_path: &PathBuf
         match component {
             Component::Prefix(p) => match p.kind() {
                 Prefix::Verbatim(osstr) | Prefix::DeviceNS(osstr) => {
-                    sub_path.push_str(osstr.to_str().unwrap_or("?"))
+                    //sub_path.push_str(osstr.to_str().unwrap_or("?"))
+                    sub_path.push_str(""); // Ignored
                 }
                 Prefix::VerbatimUNC(hostname, sharename) | Prefix::UNC(hostname, sharename) => {
                     sub_path.push_str(hostname.to_str().unwrap_or("?"));
@@ -149,8 +177,6 @@ fn create_backup_file_path(source_file_path: &PathBuf, backup_dir_path: &PathBuf
 
     backup_file_path.push(sub_path);
 
-    println!("{backup_file_path:?}");
-
     backup_file_path
 }
 
@@ -158,6 +184,7 @@ fn create_backup_file_path(source_file_path: &PathBuf, backup_dir_path: &PathBuf
 mod tests {
     use super::*;
 
+    use core::time;
     use std::fs::File;
     use std::io::{self, Write};
     //use tempfile::tempdir;
@@ -222,6 +249,8 @@ mod tests {
         let backup_path = test_dir.path().join("backup");
         let mut backup_file = File::create(backup_path)?;
         writeln!(backup_file, "Some test data")?;
+
+        std::thread::sleep(time::Duration::from_millis(250));
 
         let source_path = test_dir.path().join("source_test_data");
         let mut source_file = File::create(source_path)?;
@@ -333,5 +362,91 @@ mod tests {
             PathBuf::from("G:/Users/bob/Backup/D/Users/bob/Documents/test.txt"),
             backup_path
         );
+    }
+
+    #[test]
+    fn test_perform_new_backup() -> Result<(), std::io::Error> {
+        // Create a temporary directoy/file stucture to back up. Each file contains a with the name of the file.
+        // Structure is:
+        // TempDir
+        //   -> TestUser
+        //       -> DocumentsA
+        //          --> fileAA.txt
+        //          --> fileBA.txt
+        //       --> DocumentsB
+        //           --> fileBA.pdf
+        //           --> fileBB.doc
+        //           --> fileBC.txt
+        //       --> DocumentsC
+        //           --> (empty)
+        //
+        let test_dir = tempfile::tempdir()?;
+
+        fs::create_dir(test_dir.path().join("TestUser"))?;
+
+        fs::create_dir(test_dir.path().join("TestUser/DocumentsA"))?;
+        fs::create_dir(test_dir.path().join("TestUser/DocumentsB"))?;
+        fs::create_dir(test_dir.path().join("TestUser/DocumentsC"))?;
+
+        let mut f = File::create(test_dir.path().join("TestUser/DocumentsA/fileAA.txt"))?;
+        write!(f, "fileAA.txt")?;
+        f = File::create(test_dir.path().join("TestUser/DocumentsA/fileAB.txt"))?;
+        write!(f, "fileAB.txt")?;
+        f = File::create(test_dir.path().join("TestUser/DocumentsB/fileBA.pdf"))?;
+        write!(f, "fileBA.pdf")?;
+        f = File::create(test_dir.path().join("TestUser/DocumentsB/fileBB.doc"))?;
+        write!(f, "fileBB.doc")?;
+        f = File::create(test_dir.path().join("TestUser/DocumentsB/fileBC.txt"))?;
+        write!(f, "fileBC.txt")?;
+
+        // A few checks that te test code works
+        assert!(test_dir
+            .path()
+            .join("TestUser/DocumentsA/fileAB.txt")
+            .exists());
+
+        assert!(test_dir.path().join("TestUser/DocumentsC").exists());
+
+        // Test perform_backup()
+        perform_backup(
+            test_dir.path().join("TestUser"),
+            test_dir.path().join("Backup"),
+        );
+
+        // Check the results
+        // First get the path of the temp directory.
+        let tail = test_dir.path().to_str().unwrap().to_string();
+        // Assuming that the temp dir used for test in the C: drive. For the backup path remove
+        // the C: and replace it with C
+        let tail_norm = tail.replace(":", "");
+        // Get the full backup path, i.e.
+        // <temp test dir>/Backup/<temp test dir with C: changed to C>
+        let full_backup_path = test_dir.path().join("Backup").join(tail_norm);
+
+        assert!(test_dir.path().join("Backup").exists());
+        assert!(full_backup_path.join("TestUser/DocumentsA").exists());
+        assert!(full_backup_path.join("TestUser/DocumentsB").exists());
+        assert!(full_backup_path.join("TestUser/DocumentsC").exists());
+
+        assert!(full_backup_path
+            .join("TestUser/DocumentsA/fileAA.txt")
+            .exists());
+        assert!(full_backup_path
+            .join("TestUser/DocumentsA/fileAB.txt")
+            .exists());
+
+        assert!(full_backup_path
+            .join("TestUser/DocumentsB/fileBA.pdf")
+            .exists());
+        assert!(full_backup_path
+            .join("TestUser/DocumentsB/fileBB.doc")
+            .exists());
+        assert!(full_backup_path
+            .join("TestUser/DocumentsB/fileBC.txt")
+            .exists());
+
+        assert!(full_backup_path.join("TestUser/DocumentsC").exists());
+
+        Ok(())
     }
 }
